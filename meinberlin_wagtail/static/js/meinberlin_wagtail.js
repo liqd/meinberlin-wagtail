2b39b9bf345db890d96d10efe069d6ca1619b03f
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-03-16 16:09:51 +0100

    show username and logout

--- meinberlin_wagtail/static/js/meinberlin_wagtail.js
+++ meinberlin_wagtail/static/js/meinberlin_wagtail.js
@@ -3,3 +3,64 @@ var adhocracyOrigin = document.body.dataset.adhocracyUrl;
 adhocracy.init(adhocracyOrigin, function(adhocracy) {
     adhocracy.embed(".adhocracy_marker");
 });
+
+(function() {
+    "use strict";
+
+    var http = function(url, cb) {
+        var req = new XMLHttpRequest();
+
+        req.onreadystatechange = function(e) {
+            if (req.readyState === 4 && req.status < 400) {
+                cb(e.target.response);
+            }
+        };
+
+        req.open('GET', url, true);
+        req.send(null);
+    };
+
+    var onLogin = function(userName) {
+        document.getElementById("user-name").textContent = userName;
+        document.getElementById("user-indicator").className = "is-logged-in";
+    };
+
+    var onLogout = function() {
+        document.getElementById("user-indicator").className = "";
+    };
+
+    var getUserName = function(url, cb) {
+        var key = 'userName:' + url;
+        var userName = localStorage.getItem(key);
+        if (userName) {
+            cb(userName);
+        } else {
+            http(url, function(response) {
+                var data = JSON.parse(response);
+                var userName = data.data["adhocracy_core.sheets.principal.IUserBasic"].name;
+                localStorage.setItem(key, userName);
+                cb(userName);
+            });
+        }
+    };
+
+    var getLoginState = function() {
+        var sessionValue = localStorage.getItem("user-session");
+        if (sessionValue) {
+            var session = JSON.parse(sessionValue);
+            var url = session["user-path"];
+            getUserName(url, onLogin);
+        } else {
+            onLogout();
+        }
+    };
+
+    window.addEventListener("storage", getLoginState);
+    getLoginState();
+
+    document.getElementById("logout").addEventListener("click", function(event) {
+        event.preventDefault();
+        localStorage.removeItem("user-session");
+        onLogout();
+    });
+})();

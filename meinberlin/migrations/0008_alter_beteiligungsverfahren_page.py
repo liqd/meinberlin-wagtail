16f04482f2aada37acfd8d02411adeb40cc5e94e
Tobias Bengfort <tobias.bengfort@liqd.de>
2016-04-27 09:49:55 +0200

    disable migration 0008

--- meinberlin/migrations/0008_alter_beteiligungsverfahren_page.py
+++ meinberlin/migrations/0008_alter_beteiligungsverfahren_page.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
-
 from django.db import migrations
 
 
 def alter_beteiligungsverfahren_page(apps, schema_editor):
+    # This migration is no longer needed because it has been executed on all
+    # installations and will not change anything on new ones. It is disabled
+    # because of some unicode issue.
+    pass
 
-    SimplePage = apps.get_model('meinberlin.SimplePage')
-
-    with open('initial_content.json') as fh:
-        initial_content = json.load(fh)
-
-    for entry in initial_content:
-        if entry['pk'] == 11 and entry['model'] == 'meinberlin.simplepage':
-            page = SimplePage.objects.get(pk=11)
-            page.body = entry['fields']['body']
-            page.save()
-            break
+    # SimplePage = apps.get_model('meinberlin.SimplePage')
+    #
+    # with open('initial_content.json') as fh:
+    #     initial_content = json.load(fh)
+    #
+    # for entry in initial_content:
+    #     if entry['pk'] == 11 and entry['model'] == 'meinberlin.simplepage':
+    #         page = SimplePage.objects.get(pk=11)
+    #         page.body = entry['fields']['body']
+    #         page.save()
+    #         break
 
 
 class Migration(migrations.Migration):

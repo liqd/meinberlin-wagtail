6ab84acb832e3c9977bce98cfae92ca2b24dee6c
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-01-17 15:15:04 +0100

    adapt hero title

--- info/build.js
+++ info/build.js
@@ -21,7 +21,7 @@ var view = {
         id: 'contact',
         title: 'Kontakt',
     }],
-    heroTitle: 'mein.berlin',
+    heroTitle: 'meinBerlin',
     heroBody: 'Herzlich Willkommen bei meinBerlin, der Plattform für Bürgerbeteiligungsprozesse in Berlin!',
     introTitle: 'meinBerlin ist der Ort, an dem Sie mit Bürger*innen der Stadt ins Gespräch kommen.',
     introBody: 'Hier können Sie Meinungen erfragen oder Stellungnahmen zu Ihrem Bebauungsplanverfahren erhalten. Mein Berlin soll in Zukunft alle gesetzlich geregelten und informellen Bürgerbeteiligungsverfahren Berlins beherbergen. Es haben bereits viele Beteiligungen stattgefunden: hier können Sie mehr über verschiedene Möglichkeiten erfahren und Referenzprojekte finden.',
